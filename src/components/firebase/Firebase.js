4c9a0695905abf02998d311a266b92e9599a2992
Felipe Mosquera <felipe298@gmail.com>
2025-01-19 23:30:54 +0100

    Editando variables de entorno con vite

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -4,14 +4,15 @@ import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
